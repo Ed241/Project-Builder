46565bfed843fc6f8ae2105002301356b6198608
Edward <edward241rodriguez@gmail.com>
2023-02-17 14:51:37 -0800

    secont commit

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,35 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import projects.exception.DbException;
+
+public class DbConnection {
+
+	private static String HOST = "localhost";
+	private static String PASSWORD = "projects";
+	private static int PORT = 3306;
+	private static String SCHEMA = "projects";
+	private static String USER = "projects";
+
+	public static Connection getConnection() {
+		String url = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSl=false", HOST, PORT, SCHEMA, USER,
+				PASSWORD);
+		
+		System.out.println("Connecting with url=" + url);
+		
+		try {
+			Connection conn = DriverManager.getConnection(url);
+			System.out.println("Successfully Connected!!");
+			return conn;
+			
+			
+		} catch (SQLException e) {
+			System.out.println("Connection has failed :(");
+			throw new DbException(e);
+		}
+	}
+
+}
